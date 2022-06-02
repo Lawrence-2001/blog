5f8b003cfa1c0f1cd4551fdc5ba3f35c800a0a78
Lawrence-2001 <kan6666king@gmail.com>
2022-06-02 18:43:15 +0600

    added view sidebar.php

---
+++ view/main/sidebar.php
@@ -0,0 +1,26 @@
+<div id="sidebar" class="s-content__sidebar large-4 column">
+
+    <div class="widget widget--search">
+        <h3 class="h6">Search</h3>
+        <form action="#">
+            <input type="text" value="Search here..." onblur="if(this.value == '') { this.value = 'Search here...'; }" onfocus="if (this.value == 'Search here...') { this.value = ''; }" class="text-search">
+            <input type="submit" value="" class="submit-search">
+        </form>
+    </div>
+
+    <div class="widget widget--categories">
+        <h3 class="h6">Categories.</h3>
+        <ul>
+            <li><a href="#0" title="">Wordpress</a> (2)</li>
+            <li><a href="#0" title="">Ghost</a> (14)</li>
+            <li><a href="#0" title="">Joomla</a> (5)</li>
+            <li><a href="#0" title="">Drupal</a> (3)</li>
+            <li><a href="#0" title="">Magento</a> (2)</li>
+            <li><a href="#0" title="">Uncategorized</a> (9)</li>
+        </ul>
+    </div>
+</div> <!-- end sidebar -->
+
+</div> <!-- end row -->
+
+</div> <!-- end content-wrap -->
