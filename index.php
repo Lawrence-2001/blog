a00cf20c024d9ab6a3b443f5fae549c88b879b71
Lawrence-2001 <kan6666king@gmail.com>
2022-06-02 20:53:25 +0600

    added bootstrap.php

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
 
-include_once('helpers/db_functions.php');
-include_once('configuration/db_configuration.php');
-include_once('model/post.php');
-include_once('helpers/bufferization.php');
+include_once('configuration/bootstrap.php');
 
 $articles = getPosts($db);
 $title = 'Lawrence.com';
 
-$articlesHTML = template('posts',['articles' => $articles]);
-$sidebarHTML = template('main/sidebar');
-$pageHTML = template('main/main',['content' =>$articlesHTML, 'sidebar' => $sidebarHTML, 'title' => $title]);
+$pageHTML = buildPage('posts', ['title' => $title, 'sidebar' => 'main/sidebar'], ['articles' =>$articles]);
 
 echo $pageHTML;
