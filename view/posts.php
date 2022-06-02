b932f89aa4aa400baa968af795fd9a127e87d69b
Lawrence-2001 <kan6666king@gmail.com>
2022-06-02 19:44:08 +0600

    deleted date field in view post

--- view/posts.php
+++ view/posts.php
@@ -4,12 +4,12 @@
     <header class="entry__header">
 
         <h2 class="entry__title h1">
-            <a href="article.php?id=<?=$article['post_id']?>" title=""><?=$article['title']?></a>
+            <a href="post.php?id=<?=$article['post_id']?>" title=""><?=$article['title']?></a>
         </h2>
 
         <div class="entry__meta">
             <ul>
-                <li>July 12, 2019</li>
+                <li><!--date--></li>
                 <li><a href="#" title="" rel="category tag"><!--Category name--></a></li>
                 <li><!--Author name--></li>
             </ul>
