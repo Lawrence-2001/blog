da369b5fe8a4675dc6803b97823c85a7a17c88de
Lawrence-2001 <kan6666king@gmail.com>
2022-06-02 18:31:43 +0600

    improved view posts.php

--- view/posts.php
+++ view/posts.php
@@ -1,3 +1,32 @@
+<? foreach($articles as $id => $article): ?>
+    <article class="entry">
+
+    <header class="entry__header">
+
+        <h2 class="entry__title h1">
+            <a href="article.php?id=<?=$article['post_id']?>" title=""><?=$article['title']?></a>
+        </h2>
+
+        <div class="entry__meta">
+            <ul>
+                <li>July 12, 2019</li>
+                <li><a href="#" title="" rel="category tag">Ghost</a></li>
+                <li><!--Author name--></li>
+            </ul>
+        </div>
+
+    </header>
+
+    <div class="entry__content">
+        <p>
+            <?=$article['content']?>
+        </p>
+    </div>
+
+</article> <!-- end entry -->
+<? endforeach; ?>
+
+
 <a href="add.php">Add article</a>
 <hr>
 <div class="articles">
