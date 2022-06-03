a475f7274082576288948070769e35597deb220b
Lawrence-2001 <kan6666king@gmail.com>
2022-06-03 08:20:58 +0600

    fixed bug in configuration/bootstrap.php

--- configuration/bootstrap.php
+++ configuration/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 include_once('helpers/db_functions.php');
 include_once('helpers/bufferization.php');
-include_once('helpers/validation.php.php');
+include_once('helpers/validation.php');
 include_once('configuration/db_configuration.php');
 include_once('model/post.php');
