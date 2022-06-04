d81146ed3d8e980bc1bd1de410786ce2c963b1a0
Lawrence-2001 <kan6666king@gmail.com>
2022-06-04 17:05:39 +0600

    added new model category

---
+++ model/category.php
@@ -0,0 +1,7 @@
+<?php
+function getCategories(PDO $connection) : array {
+    $sql_query = "SELECT * FROM `categories`";
+    $result = dbQuery($connection, $sql_query);
+    return $result->fetchAll();
+}
+
