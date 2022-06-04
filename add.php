e7e3922b7ccd53dbba7234e64447e14692bbabf6
Lawrence-2001 <kan6666king@gmail.com>
2022-06-04 15:07:40 +0600

    refactored add controller and view

--- add.php
+++ add.php
@@ -1,33 +1,21 @@
 <?php
-include_once('helpers/db_functions.php');
-include_once('helpers/validation.php');
-include_once('configuration/db_configuration.php');
-include_once('model/post.php');
+include_once('configuration/bootstrap.php');
 
-$id = $_GET['id'] ?? '';
 $fieldsNames = ['content', 'title'];
 $fields = [];
 $errors = [];
-$minLengthOfField = 2;
-$view = 'edit';
+$pageParams = ['title' => 'Add article', 'sidebar' => 'main/sidebar'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fields = extractFields($_POST, $fieldsNames);
-    if (fieldsIsEntered($fields)) {
-        if (checkMinLengthOfFields($fields, $minLengthOfField)) {
-            addPost($db, $fields['title'], $fields['content']);
-            header('Location: http://localhost/hw');
-        } else {
-            $errors['checkLengthOfFields'] = "Enter at least $minLengthOfField characters";
-        }
-    } else {
-        $errors['isFieldsEntered'] = "Fill in all the fields of the form";
+    validateFormFields($fields, $errors, 2);
+    if(notExistsErrors($errors)){
+        addPost($db, $fields['title'], $fields['content']);
+        header('Location: http://localhost/hw');
     }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $fields = extractFields($_GET, $fieldsNames);
 }
 
-include_once('view/main/header.php');
-include_once('view/main/errors.php');
-include_once("view/$view.php");
-include_once('view/main/footer.php');
+$pageHTML = buildPage('add', $pageParams, ['fields' =>$fields, 'errors' => $errors]);
+echo $pageHTML;
