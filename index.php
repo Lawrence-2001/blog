ce5a6b9ff8b90167f1cc3a35475526bee3d7da97
Lawrence-2001 <kan6666king@gmail.com>
2022-06-04 18:11:50 +0600

    added output of articles of category"

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 include_once('configuration/bootstrap.php');
 
-$articles = getPosts($db);
+$articles = isset($_GET['category_id']) && !empty($_GET['category_id'])? getPostsByCategory($db, $_GET['category_id']) : getPosts($db);
+$view = $articles != false ? 'posts' : 'main/404';
 $categories = getCategories($db);
 $pageParams = ['title' => 'Lawrence.com', 'sidebar' => 'main/sidebar', 'categories' => $categories];
-$pageHTML = buildPage('posts', $pageParams, ['articles' =>$articles]);
+$pageHTML = buildPage($view, $pageParams, ['articles' =>$articles]);
 echo $pageHTML;
