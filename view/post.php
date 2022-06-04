f58b33e2a89a072370f062f7a8a0d78d609fe89a
Lawrence-2001 <kan6666king@gmail.com>
2022-06-04 17:41:31 +0600

    added output of category name in view post

--- view/post.php
+++ view/post.php
@@ -3,6 +3,8 @@
         <div class="article">
             <h1><?=escapingFields($post['title'])?></h1>
             <div><?=escapingFields($post['content'])?></div>
+            <br>
+            <div>Category: <?=escapingFields($post['name'])?></div>
             <hr>
             <a href="delete.php?id=<?=$post['post_id']?>">Remove</a>
             <br><br>
