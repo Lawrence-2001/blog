0ffc1c34c4ad3841cb27d4a92d3658fd2c658424
Lawrence-2001 <kan6666king@gmail.com>
2022-06-04 15:06:58 +0600

    added functions notExistsErrors and validateFormFields

--- helpers/validation.php
+++ helpers/validation.php
@@ -1,45 +1,65 @@
 <?php
-    function convertToUTF8(string $string) {
-        return mb_convert_encoding($string, 'UTF-8');
-    }
+function convertToUTF8(string $string)
+{
+    return mb_convert_encoding($string, 'UTF-8');
+}
 
-    function extractFields(array $sourceArray, array $keyNames) : array {
-        $result = [];
-        foreach ($keyNames as $key){
-            $result[$key] = !empty($sourceArray[$key]) ? validationField(
-                                                           convertToUTF8($sourceArray[$key])) : '';
+function extractFields(array $sourceArray, array $keyNames): array
+{
+    $result = [];
+    foreach ($keyNames as $key) {
+        $result[$key] = !empty($sourceArray[$key]) ? trim(
+            convertToUTF8($sourceArray[$key])) : '';
+    }
+    return $result;
+}
+
+function fieldsIsEntered(array $fields): bool
+{
+    $marker = true;
+    foreach ($fields as $field) {
+        if (empty($field)) {
+            $marker = false;
         }
-        return $result;
     }
+    return $marker;
+}
 
-    function fieldsIsEntered(array $fields) : bool{
-        $marker = true;
-        foreach ($fields as $field){
-            if(empty($field)){
-                $marker = false;
-            }
+;
+
+function checkMinLengthOfField(string $string, int $minLength): bool
+{
+    return mb_strlen($string, 'UTF-8') >= $minLength;
+}
+
+function checkMinLengthOfFields(array $fields, int $minLength): bool
+{
+    $marker = true;
+    foreach ($fields as $field) {
+        if (!checkMinLengthOfField($field, $minLength)) {
+            $marker = false;
         }
-        return $marker;
-    };
-
-    function validationField(string $field) : string {
-        return trim(convertToUTF8($field));
     }
+    return $marker;
+}
 
-    function checkMinLengthOfField(string $string, int $minLength) : bool {
-        return mb_strlen($string, 'UTF-8') >= $minLength;
-    }
+function escapingFields(string $field): string
+{
+    return htmlspecialchars($field);
+}
 
-    function checkMinLengthOfFields(array $fields, int $minLength) : bool {
-        $marker = true;
-        foreach ($fields as $field){
-            if(!checkMinLengthOfField($field, $minLength)){
-                $marker = false;
-            }
+function validateFormFields(array $fields, array &$errors, int $minLengthOfField)
+{
+    $errors['fieldsIsEntered'] = fieldsIsEntered($fields) ? true : "Fill in all the fields of the form";
+    $errors['checkMinLengthOfFields'] = checkMinLengthOfFields($fields, $minLengthOfField) ? true : "Enter at least $minLengthOfField characters";
+}
+
+function notExistsErrors(array $errors) : bool {
+    $marker = true;
+    foreach ($errors as $error){
+        if($error !== true){
+            $marker = false;
         }
-        return $marker;
     }
-
-    function escapingFields(string $field) : string {
-        return htmlspecialchars($field);
-    }
+    return $marker;
+}
