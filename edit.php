25aa4639cb515a7c8d8bfd7d4371ff7a719c0dae
Lawrence-2001 <kan6666king@gmail.com>
2022-06-04 15:33:42 +0600

    refactored edit controller

--- edit.php
+++ edit.php
@@ -1,44 +1,31 @@
 <?php
-include_once('helpers/db_functions.php');
-include_once('helpers/validation.php');
-include_once('configuration/db_configuration.php');
-include_once('model/post.php');
+include_once('configuration/bootstrap.php');
 
 $id = $_GET['id'] ?? '';
 $fieldsNames = ['content', 'title'];
 $fields = [];
 $errors = [];
-$lengthOfField = 2;
-$view = 'edit';
+
+$view = 'articleForm';
+$pageParams = ['title' => 'Add article', 'sidebar' => 'main/sidebar'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fields = extractFields($_POST, $fieldsNames);
-    if (fieldsIsEntered($fields)) {
-        if (checkMinLengthOfFields($fields, $lengthOfField)) {
-            editPost($db, $id, $fields['title'], $fields['content']);
-            header('Location: http://localhost/hw');
-        } else {
-            $errors['checkLengthOfFields'] = "Enter at least $lengthOfField characters";
-        }
-    } else {
-        $errors['isFieldsEntered'] = "Fill in all the fields of the form";
+    validateFormFields($fields, $errors, 2);
+    if (notExistsErrors($errors)) {
+        editPost($db, $id, $fields['title'], $fields['content']);
+        header('Location: http://localhost/hw');
     }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (!empty($id)) {
-        $post = getPost($db, $id);
-        if ($post) {
-            $fields = extractFields($post, ['title', 'content']);
-            $view = 'edit';
-        } else {
-            $view = 'main/404';
-            header('HTTP/1.1 404 NOT FOUND');
-        }
+    $post = getPost($db, $id);
+    if ($post != false) {
+        $fields = extractFields($post, ['title', 'content']);
+        $view = 'edit';
+    } else {
+        $view = 'main/404';
+        header('HTTP/1.1 404 NOT FOUND');
     }
 }
 
-
-
-include_once('view/main/header.php');
-include_once('view/main/errors.php');
-include_once("view/$view.php");
-include_once('view/main/footer.php');
+$pageHTML = buildPage($view, $pageParams, ['fields' => $fields, 'errors' => $errors]);
+echo $pageHTML;
