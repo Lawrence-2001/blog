2027d373284d4108488cf44bb2bf748dca90ca5b
Lawrence-2001 <kan6666king@gmail.com>
2022-06-04 17:07:21 +0600

    added model category on bootstrap

--- configuration/bootstrap.php
+++ configuration/bootstrap.php
@@ -4,3 +4,4 @@ include_once('helpers/bufferization.php');
 include_once('helpers/validation.php');
 include_once('configuration/db_configuration.php');
 include_once('model/post.php');
+include_once('model/category.php');
