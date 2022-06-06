bd40ba1c2585af6b0e4c68bc930e388f65b1e77f
Lawrence-2001 <kan6666king@gmail.com>
2022-06-06 13:02:49 +0600

    refactored add.php

--- add.php
+++ add.php
@@ -6,9 +6,11 @@ $fields = [];
 $errors = [];
 $categories = getCategories($db);
 $pageParams = ['title' => 'Lawrence.com', 'sidebar' => 'main/sidebar', 'categories' => $categories];
+$category_id = NULL;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fields = extractFields($_POST, $fieldsNames);
+    $category_id = $_POST['category_id'];
     validateFormFields($fields, $errors, 2);
     if(notExistsErrors($errors)){
         addPost($db, $fields['title'], $fields['content'], $_POST['category_id']);
@@ -18,5 +20,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fields = extractFields($_GET, $fieldsNames);
 }
 
-$pageHTML = buildPage('articleForm', $pageParams, ['fields' =>$fields,'categories' => $categories, 'errors' => $errors]);
+$pageHTML = buildPage('articleForm', $pageParams, ['fields' =>$fields,'categories' => $categories, 'errors' => $errors, 'category_id' => $category_id]);
 echo $pageHTML;
--- view/articleForm.php
+++ view/articleForm.php
@@ -7,18 +7,19 @@
 <div class="form">
     <form method="post">
         Article Title:<br>
-        <input type="text" name="title" value="<?=escapingFields($fields['title'])?>"><br><br>
+        <input type="text" name="title" value="<?= escapingFields($fields['title']) ?>"><br><br>
         Article Content:<br>
-        <input type="text" name="content" value="<?=escapingFields($fields['content'])?>"><br><br>
+        <input type="text" name="content" value="<?= escapingFields($fields['content']) ?>"><br><br>
         Category:<br>
         <select name="category_id">
-        <? foreach($categories as $category): ?>
-            <option
-                    value="<?=$category['category_id']?>"
-            >
-                <?=$category['name']?>
-            </option>
-        <? endforeach; ?>
+            <? foreach ($categories as $category): ?>
+                <option
+                        value="<?= $category['category_id'] ?>"
+                    <? echo(selectOption($category['category_id'], $category_id)); ?>
+                >
+                    <?= $category['name'] ?>
+                </option>
+            <? endforeach; ?>
         </select>
         <button>Send</button>
     </form>
